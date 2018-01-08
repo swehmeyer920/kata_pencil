4c10245179590a78087dfee29028b97366d9a23d
swehmeyer920 <stephen.wehmeyer@gmail.com>
2018-01-08 15:02:00 -0500

    Added erase test and code

--- kata_pen/Pencil.cs
+++ kata_pen/Pencil.cs
@@ -10,7 +10,7 @@ namespace kata_pen
     {
         public int length = 0;
         // The sheet of paper we're writing on.
-        string sheet;
+        private string sheet;
 
         private int _durability;
         // durability of a sharpened point.
@@ -29,6 +29,9 @@ namespace kata_pen
 
         private int currentDurability = 4000;
 
+        /// <summary>
+        /// Sharpen the pencil point and decrease it's length.
+        /// </summary>
         public void sharpen()
         {
             if (length > 0)
@@ -36,6 +39,50 @@ namespace kata_pen
                 currentDurability = _durability;
                 --length;
             }
+        }
+        /// <summary>
+        /// Erase the passed in text from the sheet.
+        /// </summary>
+        /// <param name="textToErase"></param>
+        public void erase(string textToErase)
+        {
+            int lastIndex = indexOfLast(textToErase);
+            if (lastIndex == -1)
+                return;
+            sheet = sheet.Remove(lastIndex, textToErase.Length);
+            for (int i = 0; i < textToErase.Length; ++i)
+                sheet = sheet.Insert(lastIndex, " ");
+        }
+
+        /// <summary>
+        /// Find last occurance of pass in string.
+        /// </summary>
+        /// <param name="text">
+        /// String to search for.
+        /// </param>
+        /// <returns>
+        /// The index of the last occurance of the passed in text string or -1 if there are no occurances.
+        /// </returns>
+        private int indexOfLast(string text)
+        {
+            int lastLocation = 0;
+            int location = 0;
+            
+            do
+            {
+                // Set lastLocation to -1 for the first run incase
+                // the text isn't found at all.
+                lastLocation = location == 0 ? -1 : location;
+                // We are searching just after our last search find, but
+                // if that is beyond the end of the sheet than we've already
+                // found the last text (or none existed).
+                if (location + 1 + text.Length > sheet.Length)
+                    location = -1;
+                else
+                    location = sheet.IndexOf(text, location + 1);
+            } while (location >= 0);
+
+            return lastLocation;
         }
         /// <summary>
         /// Write on paper.
